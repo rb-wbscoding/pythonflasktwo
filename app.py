5c4e5e0380f54dc0cf6ecf346e576bbcd9072c61
richard boucher <rb.wbscoding@gmail.com>
2021-03-12 23:04:46 +0100

    Besser Deutsch?

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-#app.py
+# app.py
 # Importieren von notige Flask Elementen - Flask ist eine Server Framework für die Client
-from flask import Flask, render_template, request, json, session, url_for
+from flask import Flask, render_template, request, json, session
 from flask_session import Session
 import requests
 
@@ -9,31 +9,30 @@ import os
 keyinfo = os.getenv("KEY_INFO")
 
 app = Flask(__name__)
-#session key
-app.secret_key="richard"
+# session key
+app.secret_key = "richard"
 # sollte auf server gespeichert (lokalle Festplatte) und für app verfügbar
 SESSION_TYPE = "filesystem"
 app.config.from_object(__name__)
 Session(app)
 
 # Variable Deklaration und Initialisierung
-displayed = False
-picturedataarray=[]
+picturedataarray = []
 
 # Bei anlauf des Program sind diesen Aktion gemacht
 # Sie sind eine Dictionary von mögliche Bilder Addressen, 3 mal Geholt
 @app.before_first_request
 def load_from_API():
-    # Was gesucht sein sollen - API schlussel (in .env File), suchen nach donald trump, alle Bild typen und nur 10 Antworten
+    # Was ist gesucht - API schlussel (in .env File), suchen nach donald trump, alle Bildtypen und nur 10 Antworten
     parameters = {
         'key': keyinfo,
         'q': 'donald+trump',
         'image_type': 'all',
         'per_page': 10,
     }
-    #Abholung von den Bilder Info und Addressen bie pixabay
+    # Abholung von den Bilderinfo und Addressen bie pixabay
     responsedon = requests.get('https://pixabay.com/api/', params=parameters)
-    respdon=responsedon.json()
+    respdon = responsedon.json()
 
     # Gleichen wie oben nür mit Putin und Merkel
     parameters = {
@@ -53,36 +52,41 @@ def load_from_API():
     }
     responseangie = requests.get('https://pixabay.com/api/', params=parameters)
     respangie = responseangie.json()
-    #Als session Data gespeichert - Information von Bilder
+    # Als session Data gespeichert - Information von Bilder
     session['API_data'] = [respdon['hits'], respput['hits'], respangie['hits']]
 
 # Base route für Server - sendet die möglicher Bilder Addresse auf die Client nür wann es gibt Datei in session
 @app.route('/')
 def load_up_choice():
     if "API_data" in session:
-        return render_template('demo.html', respdon=session["API_data"][0], respput=session["API_data"][1], respangie=session["API_data"][2])
+        return render_template('demo.html', respdon=session["API_data"][0], respput=session["API_data"][1],
+                               respangie=session["API_data"][2])
     else:
         return 'Something has gone wrong, please refresh'
 
-#nach die Wahl von eine Bild ist die addresse zu den display template geschicht
-#benutzung von 'apply' macht die Text in den Bild und gibt eine Änderung in den Display Variable
+# nach die Wahl von eine Bild ist die addresse zu den display template geschicht
+# benutzung von 'apply' macht die Text in den Bild und gibt eine Änderung in den Display Variable
 @app.route('/display/', methods=['POST', 'GET'])
 def displ():
-    if request.method=='POST':
+    if request.method == 'POST':
         if "submit-img" in request.form:
             myformdata = request.form["submit-img"]
             # Muss die getauscht zwischen " und ' (Python problem?)
             myjsoncompatibility = myformdata.replace("'", '"')
             # In session data gespeichert
             jsoneddata = json.loads(myjsoncompatibility)
-            session['picdata']=jsoneddata
+            session['picdata'] = jsoneddata
+            # Variable Deklaration und Initialisierung
             displayed = False
             # Gewählte Bilder Data zu Template geschicht
-            return render_template('display.html', jsoneddata=jsoneddata, topText="", bottomText="", textColor="black", textSize="30", displayed=displayed)
+            return render_template('display.html', jsoneddata=jsoneddata, topText="", bottomText="",
+                                   textColor="black", textSize="30", displayed=displayed)
         elif "apply" in request.form:
             # Text Data in Bild verbunden
             displayed = True
-            return render_template('display.html', jsoneddata=session['picdata'], topText=request.form["topText"], bottomText=request.form["bottomText"], textColor=request.form["textColor"], textSize=request.form["textSize"], displayed=displayed)
+            return render_template('display.html', jsoneddata=session['picdata'], topText=request.form["topText"],
+                                   bottomText=request.form["bottomText"], textColor=request.form["textColor"],
+                                   textSize=request.form["textSize"], displayed=displayed)
 
 # kann mann mit Wiederhollung nür deine ausgewählte Bild sehen
 # Läuft wann Try-Again button benutzt
@@ -95,7 +99,7 @@ def displ():
 # Route 'Saved' Bildern anzuschauen
 @app.route('/saved/', methods=['POST', 'GET'])
 def saveInterface():
-    if request.method=='POST':
+    if request.method == 'POST':
         # JSON in normaller Data umwandeln
         pictureJSON = request.form['picturedata']
         picturedata = json.loads(pictureJSON)
@@ -104,7 +108,7 @@ def saveInterface():
         picturedata = picturedata.replace(' ', '+')
 
         # Kann nur die neuste 10 Bild in die Session zeigen
-        lengthOfArray=len(picturedataarray)
+        lengthOfArray = len(picturedataarray)
         if lengthOfArray > 10:
             del picturedataarray[0]
 
@@ -116,7 +120,7 @@ def saveInterface():
 # Wann von Saved Button in Header
     else:
         # Array von Pic data in Template eingeladen.
-        return render_template('allsaved.html', savedMeme=picturedataarray )
+        return render_template('allsaved.html', savedMeme=picturedataarray)
 
 if __name__ == '__main__':
     app.run(debug=True)
